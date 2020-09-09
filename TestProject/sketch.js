e59d92b876685d4a8fc51760b1ab0f51832c0f8c
scotchANDsolder <narulaab@msu.edu>
2020-09-09 16:12:30 -0400

    1 - Changed my welcome page 2 - Changed circle color 3 - Changed canvas size

--- TestProject/sketch.js
+++ TestProject/sketch.js
@@ -1,14 +1,14 @@
 
 function setup() {
   // put setup code here
-createCanvas(200,200);
+createCanvas(400,400);
 background(200,10,120);
 
 }
 
 function draw() {
   //Put draw code here
-  fill(30,40,200);
+  fill(200,0,0);
   circle(100,100,70);
 
 
