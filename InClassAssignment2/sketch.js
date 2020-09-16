e7b8d12e04e0dbc8a0274e5249fbe439c06f0fa6
narulaab <narulaab@msu.edu>
2020-09-16 17:07:59 -0400

    Added in class assignment 2!

---
+++ InClassAssignment2/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+
+  createCanvas(windowWidth,windowHeight);
+
+}
+function draw() {
+
+  background(200,10,120);
+  fill(0,200,0);
+  rect(500,500,100);
+
+
+
+
+
+}
--- TestProject/sketch.js
+++ TestProject/sketch.js
@@ -1,15 +1,36 @@
+let xpos = 200;
+let ypos = 300;
+let ballspeed = 1;
 
 function setup() {
 
   createCanvas(windowWidth,windowHeight);
+
 }
 function draw() {
 
   background(200,10,120);
-  fill(0,255,0);
-  if( dist(mouseX,mouseY,width/2,height/2) < 100 ){
-    circle(width/2,height/2,100);
+  fill(0,0,200);
+  circle(xpos,ypos,100);
+
+  if(keyIsPressed & keyCode === UP_ARROW){
+    ypos = ypos - ballspeed;
   }
+
+  if(keyIsPressed & keyCode === DOWN_ARROW){
+    ypos = ypos + ballspeed;
+  }
+
+  if(keyIsPressed & keyCode === LEFT_ARROW){
+    xpos = xpos - ballspeed;
+  }
+  if(keyIsPressed & keyCode === RIGHT_ARROW){
+    xpos = xpos + ballspeed;
+  }
+
+
+
+
 
 
 }
