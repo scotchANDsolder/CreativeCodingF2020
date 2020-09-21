a45b0175a0743b14f5d9abd91593bcee2ff00de1
narulaab <narulaab@msu.edu>
2020-09-21 18:13:11 -0400

    Updated test project

--- Inclassassignment2/sketch.js
+++ Inclassassignment2/sketch.js
@@ -1,15 +1,24 @@
-function setup() {
+let checker = 30;
 
-  createCanvas(windowWidth,windowHeight);
+
+function setup() {
+  // put setup code here
+  createCanvas(200,200);
+  background(200,10,120);
+
 
 }
+
 function draw() {
-
-  background(200,10,120);
-  fill(0,255,200);
-  rect(500,500,400)
-
-
+  //Put draw code here
+  if(mouseIsPressed & checker > 50){
+    fill(255,0,0);
+    circle(100,50,20);
+  }
+  else {
+    fill(0,255,0);
+    rect(10,20,40);
+  }
 
 
 
